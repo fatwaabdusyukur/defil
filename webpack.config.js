86952cdb91e6ce862dc1f208e13a963704f28971
fatwaabdusyukur <waengyoo@gmail.com>
2024-04-17 17:45:40 +0700

    Initialize and configure project

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const { resolve } = require("path");
+const { argv } = require("process");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const isProduction = argv[3] === "production";
+
+const fileExtensions = [
+  "jpg",
+  "jpeg",
+  "png",
+  "gif",
+  "eot",
+  "otf",
+  "svg",
+  "ttf",
+  "woff",
+  "woff2",
+];
+
+module.exports = {
+  mode: isProduction ? "production" : "development",
+  devtool: isProduction ? "source-map" : "cheap-module-source-map",
+  entry: {
+    content: resolve(__dirname, "src/content/content.js"),
+  },
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: new RegExp(`.(${fileExtensions.join("|")})$`),
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/manifest.json", to: "manifest.json" },
+        { from: "src/assets/img/logo.png", to: "img/logo.png" },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+};
